d274880bcd6092a8eef1697033925c168cbc9d61
Jordy Rymenants <jordy.rymenants@outlook.com>
2023-01-13 15:35:07 +0100

    docs(button): story per prop

--- src/components/Button.stories.tsx
+++ src/components/Button.stories.tsx
@@ -10,19 +10,71 @@ const Template: StoryFn<typeof Button> = (args) => (
   <Button onClick={() => action("click")} {...args} />
 );
 
-export const Primary = Template.bind({});
-Primary.args = {
-  block: false,
-  children: "Primary button",
-  disabled: false,
-  loading: false,
-  outline: false,
-  size: "md",
-  variant: "primary",
-};
+export const Variants = () => (
+  <div className="flex flex-wrap gap-1">
+    <Button variant="primary">Primary button</Button>
+    <Button variant="secondary">Secondary button</Button>
+    <Button variant="accent">Accent button</Button>
+    <Button variant="error">Error button</Button>
+    <Button variant="info">Info button</Button>
+    <Button variant="success">Success button</Button>
+    <Button variant="warning">Warning button</Button>
+    <Button variant="link">Link button</Button>
+  </div>
+);
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-  ...Primary.args,
-  variant: "secondary",
-};
+export const Block = () => (
+  <div className="flex flex-wrap gap-1">
+    <Button block>Block button</Button>
+  </div>
+);
+
+export const Disabled = () => (
+  <div className="flex flex-wrap gap-1">
+    <Button disabled>Disabled button</Button>
+  </div>
+);
+
+export const Loading = () => (
+  <div className="flex flex-wrap gap-1">
+    <Button loading>Loading button</Button>
+  </div>
+);
+
+export const Outline = () => (
+  <div className="flex flex-wrap gap-1">
+    <Button variant="primary" outline>
+      Primary outline button
+    </Button>
+    <Button variant="secondary" outline>
+      Secondary outline button
+    </Button>
+    <Button variant="accent" outline>
+      Accent outline button
+    </Button>
+    <Button variant="error" outline>
+      Error outline button
+    </Button>
+    <Button variant="info" outline>
+      Info outline button
+    </Button>
+    <Button variant="success" outline>
+      Success outline button
+    </Button>
+    <Button variant="warning" outline>
+      Warning outline button
+    </Button>
+    <Button variant="link" outline>
+      Link outline button
+    </Button>
+  </div>
+);
+
+export const Sizes = () => (
+  <div className="flex flex-wrap gap-1">
+    <Button size="xs">Extra small button</Button>
+    <Button size="sm">Small button</Button>
+    <Button size="md">Medium button</Button>
+    <Button size="lg">Large button</Button>
+  </div>
+);
