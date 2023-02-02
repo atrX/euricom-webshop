908156f0045adfd63b0453fde55f1405e496952a
Jordy Rymenants <jordy.rymenants@outlook.com>
2023-02-02 08:40:42 +0100

    test(dialog): confirmation dialog unit tests

--- src/components/DialogProvider.spec.tsx
+++ src/components/DialogProvider.spec.tsx
@@ -17,30 +17,63 @@ describe("DialogProvider component", () => {
     render(<HookWrapper />);
   }
 
-  it("should show a dialog when invoked", async () => {
-    const TestComponent: React.FC = () => {
-      return <div>Hello world!</div>;
-    };
+  describe("showDialog", () => {
+    it("should show a dialog when invoked", async () => {
+      const TestComponent: React.FC = () => {
+        return <div>Hello world!</div>;
+      };
 
-    renderDialogProvider();
+      renderDialogProvider();
 
-    void hook.showDialog(<TestComponent />);
+      void hook.showDialog(<TestComponent />, { title: "My title" });
 
-    await screen.findByText("Hello world!");
+      await screen.findByRole("heading", { name: /my title/i });
+      screen.getByText("Hello world!");
+    });
+
+    it("should dismiss the dialog when close is called and return a value", async () => {
+      const TestComponent: React.FC<{
+        close?: (result: boolean) => void;
+      }> = ({ close }) => {
+        return <Button onClick={() => close?.(true)}>Close</Button>;
+      };
+
+      renderDialogProvider();
+
+      const result = hook.showDialog(<TestComponent />);
+      const closeButton = await screen.findByRole("button", { name: /close/i });
+      await userEvent.click(closeButton);
+      expect(await result).toBe(true);
+    });
   });
 
-  it("should dismiss the dialog when close is called and return a value", async () => {
-    const TestComponent: React.FC<{
-      close?: (result: boolean) => void;
-    }> = ({ close }) => {
-      return <Button onClick={() => close?.(true)}>Close</Button>;
-    };
+  describe("showConfirmation", () => {
+    it("should show a confirmation dialog when invoked", async () => {
+      renderDialogProvider();
 
-    renderDialogProvider();
+      void hook.showConfirmation("Hello world!", { title: "My title" });
 
-    const result = hook.showDialog(<TestComponent />);
-    const closeButton = await screen.findByRole("button", { name: /close/i });
-    await userEvent.click(closeButton);
-    expect(await result).toBe(true);
+      await screen.findByRole("heading", { name: /my title/i });
+      screen.getByRole("button", { name: /yes/i });
+      screen.getByRole("button", { name: /no/i });
+    });
+
+    it("should dismiss the dialog when No is clicked and return false", async () => {
+      renderDialogProvider();
+
+      const result = hook.showConfirmation("Hello world!");
+      const noButton = await screen.findByRole("button", { name: /no/i });
+      await userEvent.click(noButton);
+      expect(await result).toBeFalsy();
+    });
+
+    it("should dismiss the dialog when Yes is clicked and return true", async () => {
+      renderDialogProvider();
+
+      const result = hook.showConfirmation("Hello world!");
+      const yesButton = await screen.findByRole("button", { name: /yes/i });
+      await userEvent.click(yesButton);
+      expect(await result).toBeTruthy();
+    });
   });
 });
