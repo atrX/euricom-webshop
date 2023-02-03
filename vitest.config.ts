f54576906379de373297ab2c092cf5a68b5271b5
Jordy Rymenants <jordy.rymenants@outlook.com>
2023-02-03 09:25:23 +0100

    chore: fix vitest not running

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,4 +7,12 @@ export default defineConfig({
     environment: "jsdom",
     setupFiles: "./tests/setup.ts",
   },
+  resolve: {
+    alias: {
+      // fixes vitest not recognizing fs module
+      // no clue why it suddenly broke out of nowhere
+      // was working fine the day before and then stopped working without any changes...
+      fs: require.resolve("rollup-plugin-node-builtins"),
+    },
+  },
 });
